20d298f80ed44092a80170777c030c7df6cf625e
MathiusMendoza <mendozamathius9@gmail.com>
2023-11-23 17:28:10 -0500

    neww123456

--- EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo09.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo09.java
@@ -51,6 +51,8 @@ public class Ejemplo09 {
         } while (bandera); // (bandera==true)
 
         // promedio = suma / contador_calificaciones;
+        // En la linea 55 adelante de suma decimos que hacemos un casting, es un metodo 
+        // para cambiar de entero a decimal.
         promedio = (double) suma / contador_calificaciones;
         cadenaFinal = String.format("%s\nPromedio de calificaciones: %.2f",
                 cadenaFinal,
--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo09.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo09.java
@@ -22,27 +22,38 @@ public class Ejemplo09 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        
+
         String cadenaFinal;
         int nota;
         int salida;
-        String nombre; // *
+        // *
         double promedio;
         boolean bandera = true;
         int suma = 0;
         int contador_calificaciones = 0;
-        
+
         cadenaFinal = "Listado de Notas\n";
-        
+
         do {
-            
-            
+            System.out.println("Ingrese su nombre por favor");
+            entrada.nextLine();
+            System.out.println("Ingrese su apellido por favor");
+            entrada.nextLine();
+
+            System.out.println("Ingrese primer valor, solo valor entero");
+            nota = entrada.nextInt();
+            System.out.println("Ingrese segundo valor, solo valor entero");
+            nota = entrada.nextInt();
+
             // agrego valor al acumulador
             suma = suma + nota;
             // agrego una unidad al contador para luego sacar el promedio
             contador_calificaciones = contador_calificaciones + 1;
-            
-            
+
+            cadenaFinal = String.format("%sCalificación: %d\n",
+                    cadenaFinal,
+                    nota);
+
             System.out.println("Ingrese (-111) si desea salir del ciclo; "
                     + "cualquier otro número para continuar");
             salida = entrada.nextInt();
@@ -52,13 +63,13 @@ public class Ejemplo09 {
             }
             // atención
             entrada.nextLine(); // se limpia el buffer, pues el primer valor
-                               // que se solicita al inicio del ciclo es una
-                               // cadena
+            // que se solicita al inicio del ciclo es una
+            // cadena
 
         } while (bandera); // (bandera==true)
-        
+
         // promedio = suma / contador_calificaciones;
-        promedio = (double)suma / contador_calificaciones;
+        promedio = (double) suma / contador_calificaciones;
         cadenaFinal = String.format("%s\nPromedio de calificaciones: %.2f",
                 cadenaFinal,
                 promedio);
